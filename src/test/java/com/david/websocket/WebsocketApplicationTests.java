1fc8d90d4d949a42c2a781b4b621102e562f1ef8
ITSupport <diwas.subedi3@gmail.com>
2019-11-08 15:56:36 -0500

    First Commit

---
+++ src/test/java/com/david/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
